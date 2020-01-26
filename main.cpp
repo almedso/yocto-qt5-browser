5c234a6a2796ede7cdae30806d9f7e6c0e06fc87
Volker Kempert <volker.kempert@almedso.de>
2020-01-26 21:38:36 +0100

    Remove QML dependency - and add url as parameter to start with

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QtWebEngine>
+#include <QApplication>
+#include <QWebEngineView>
+
+QUrl commandLineUrlArgument()
+{
+    const QStringList args = QCoreApplication::arguments();
+    for (const QString &arg : args.mid(1)) {
+        if (!arg.startsWith(QLatin1Char('-')))
+            return QUrl::fromUserInput(arg);
+    }
+    return QUrl(QStringLiteral("http://localhost"));
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,19 +17,14 @@ int main(int argc, char *argv[])
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setOrganizationName("Qt5-Browser-Single-View-app");
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication app(argc, argv);
 
-    QGuiApplication app(argc, argv);
-
-    QtWebEngine::initialize();
-
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    QWebEngineView view;
+    view.setUrl(commandLineUrlArgument());
+    view.resize(800, 480);
+    view.show();
 
     return app.exec();
 }
--- qt5-browser.pro
+++ qt5-browser.pro
@@ -1,9 +1,4 @@
-QT += quick webengine
-CONFIG += c++11
-
-#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
-
-CONFIG += link_pkgconfig
+TEMPLATE = app
 
 # keyboard should be within the application
 CONFIG += disable-desktop
@@ -13,16 +8,11 @@ static {
     QTPLUGIN += qtvirtualkeyboardplugin
 }
 
+
+QT += webenginewidgets
+
 SOURCES += main.cpp
-
-RESOURCES += qml.qrc
-
-# Additional import path used to resolve QML modules in Qt Creator's code model
-QML_IMPORT_PATH =
-
-# Additional import path used to resolve QML modules just for Qt Quick Designer
-QML_DESIGNER_IMPORT_PATH =
-
 # Default rules for deployment.
+
 target.path = /usr/bin
 INSTALLS += target
